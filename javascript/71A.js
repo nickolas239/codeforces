f8cbf3e7265f740345f6b526a7282b717d498028
z232253 <julia.lamouche@spread.com.br>
2022-10-18 19:41:04 -0300

    adição exe 71A

---
+++ javascript/71A.js
@@ -0,0 +1,14 @@
+var a = prompt() //qtd de palavras  (ou readline)
+var b = parseInt(a) //converter string para int
+
+for (var i=0;i<b;i++) { //laço de repetição, enquanto a condicao for vdd
+    var palavra = prompt("digite a palavra:")
+    var carac = palavra.length //int pega o tamanho de caracteres da palavra
+    var abrev 
+    if (carac >10) {
+        abrev = palavra[0]+(carac-2).toString()+palavra[carac-1]  //.toString() - converte num para string 
+        console.log(abrev)
+    } else {
+        console.log(palavra)
+    }
+}
